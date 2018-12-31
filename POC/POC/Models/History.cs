0ef1bb83278a31be6a48806472e24658fc9e34bd
nidhisood9893 <nidhisood9893@gmail.com>
2019-01-01 00:21:01 +0530

    poc

---
+++ POC/POC/Models/History.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POC
+{
+   public class History
+    {
+        public string Title { get; set; }
+        public int Leaves { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public ApprovalStatus Status { get; set; }
+    }
+
+    public enum ApprovalStatus
+    {
+        Approved,
+        Pending,
+        Rejected
+    }
+}
--- POC/POC/ViewModels/AboutViewModel.cs
+++ POC/POC/ViewModels/AboutViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 using Xamarin.Forms;
@@ -7,11 +8,61 @@ namespace POC.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        public ObservableCollection<History> LeavesHistory { get; set; }
+
         public AboutViewModel()
         {
-            Title = "About";
-
+            Title = "EMP24012";
+            LeavesHistory = new ObservableCollection<History>();
+            PopulateStaticData();
             OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+        }
+
+        /// <summary>
+        /// Populating static data
+        /// </summary>
+        private void PopulateStaticData()
+        {            
+            LeavesHistory.Add(new History
+            {
+                Title="Casual Leaves",
+                Leaves = 3,
+                FromDate = "22 Aug 2018",
+                ToDate = "24 Aug 2018",
+                Status=ApprovalStatus.Pending
+            });
+            LeavesHistory.Add(new History
+            {
+                Title="Comp off",
+                Leaves = 1,
+                FromDate = "18 Jul 2018",
+                ToDate = "18 Jul 2018",
+                Status = ApprovalStatus.Rejected
+            });
+            LeavesHistory.Add(new History
+            {
+                Title="Sick Leave",
+                Leaves = 1,
+                FromDate = "06 Jun 2018",
+                ToDate = "06 Jun 2018",
+                Status = ApprovalStatus.Approved
+            });
+            LeavesHistory.Add(new History
+            {
+                Title="Leave without Pay",
+                Leaves = 6,
+                FromDate = "5 Feb 2018",
+                ToDate = "10 Feb 2018",
+                Status = ApprovalStatus.Approved
+            });
+            LeavesHistory.Add(new History
+            {
+                Title = "Casual Leaves",
+                Leaves = 4,
+                FromDate = "02 Jan 2018",
+                ToDate = "06 Jan 2018",
+                Status = ApprovalStatus.Approved
+            });
         }
 
         public ICommand OpenWebCommand { get; }
